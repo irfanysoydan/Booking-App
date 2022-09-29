b486882d05a2a8e33ded9bf24db953dad6898387
irfanysoydan <irfanysoydan@gmail.com>
2022-09-29 13:07:27 +0300

    first commit

---
+++ api/index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import hotelRoute from "./routes/hotels.js";
+import roomRoute from "./routes/rooms.js";
+import userRoute from "./routes/users.js";
+
+const app = express();
+app.use(express.json());
+dotenv.config();
+
+app.listen(7777, () => {
+  console.log("Server is running on port: 7777");
+
+  mongoose
+    .connect(process.env.CONNECTION_STRING)
+    .then(() => {
+      console.log("Database connection is succesfull");
+    })
+    .catch((err) => {
+      console.log("Can`t connect to DB");
+    });
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("DB is disconnected ");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/hotels", hotelRoute);
+app.use("/api/rooms", roomRoute);
+app.use("/api/users", userRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+
+  return res.status(500).json();
+});
---
+++ api/routes/hotels.js
@@ -0,0 +1,24 @@
+import express from "express";
+import {
+  createHotel,
+  deleteHotel,
+  getAllHotel,
+  getHotel,
+  updateHotel,
+} from "../controllers/hotels.js";
+import Hotel from "../models/Hotel.js";
+
+const router = express.Router();
+
+//CREATE
+router.post("/", createHotel);
+//UPDATE
+router.put("/:id", updateHotel);
+//DELETE
+router.delete("/:id", deleteHotel);
+//GET
+router.get("/find/:id", getHotel);
+//GET ALL
+router.get("/", getAllHotel);
+
+export default router;
